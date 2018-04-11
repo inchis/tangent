3c0a7a2b6f935e3edd6616577fef5de263f559c0
Nkosikhona <carlosnkosikhona@gmail.com>
2018-04-11 05:12:17 +0200

    04-04-2018 (First Edit)

--- tang/views.py
+++ tang/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 from tang.models import Employee
-from serializers import EmployeeSerializer
+from serializers import EmployeeSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework.test import APIRequestFactory
 from rest_framework.permissions import IsAdminUser
@@ -18,22 +18,21 @@ class UserList(generics.ListCreateAPIView):
     permission_classes = (IsAdminUser,)
 
     def get_paginate_by(self):
-        """
-        Use smaller pagination for HTML representations.
-        """
         if self.request.accepted_renderer.format == 'html':
             return 20
         return 100
 
     def list(self, request):
-        # Note the use of `get_queryset()` instead of `self.queryset`
         context = dict(request=APIRequestFactory().get('/'))
         queryset = self.get_queryset()
         serializer = EmployeeSerializer(queryset, context=context, many=True)
         return Response(serializer.data)
 
 
-class
+class UserMe(APIView):
+    serializer_class = UserSerializer
 
-
-
+    def get(self, request):
+        context = dict(request=APIRequestFactory().get('/'))
+        serializer = UserSerializer(request.user, context=context)
+        return Response(serializer.data)
--- urls.py
+++ urls.py
@@ -2,19 +2,20 @@ from rest_framework.authtoken import views
 from django.conf.urls.defaults import *
 from django.views.generic import TemplateView
 from django.contrib import admin
-from tang.views import UserList
+from tang.views import UserList, UserMe
 from rest_framework.urlpatterns import format_suffix_patterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', UserList.as_view()),
-                       url(r'^gui/$', UserList_gui.as_view()),
+                       url(r'^api/user/me/$', UserMe.as_view()),
+                       url(r'^gui/$', TemplateView.as_view(template_name="index.html")),
                        url(r'^api-token-auth/', views.obtain_auth_token),
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       #url(r'^$', 'tang.views.UserList', name="dashboard"),
                        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
